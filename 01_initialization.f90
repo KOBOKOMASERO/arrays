3e0424b8b572b4443aed8f29c704d40bee6ec467
KOBOKOMASERO <kobokophilip@gmail.com>
2025-06-25 05:28:42 +0300

    01_initialization.f90

---
+++ 01_initialization.f90
@@ -0,0 +1,22 @@
+! can be done in three ways.
+
+program array_initialization
+    implicit none
+    
+    ! using array constructor
+    integer, dimension(5) :: arr = (/1, 2, 3, 4, 5/)
+
+    !using data statement
+    integer :: arr2(5) 
+    
+    ! using do loop to populate the array
+    
+    data arr2 /1, 2, 3, 4, 5/ 
+    integer :: arr3(5)
+    arr3 = (/ (i, i=1, 5) /) 
+    
+    print *, "arr = ", arr
+    print *, "arr2 = ", arr2
+    print *, "arr3 = ", arr3
+    
+end program array_initialization
